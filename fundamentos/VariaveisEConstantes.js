9d9e7f4d40bb9b376e4bb93b5a838cd0a8694b94
Victor Amaral Guimarães de Lima Souza <amaralvitor580@gmail.com>
2025-07-21 17:25:19 -0300

    Variaveis,Constantes e Tipagem fraca

---
+++ fundamentos/VariaveisEConstantes.js
@@ -0,0 +1,17 @@
+var a = 3
+let b = 4
+
+var a = 40
+b = 30 //Nao é possivel redeclarar uma variavel let
+
+console.log(a, b)
+
+a = 300 // É possivel reatribuir o valor de uma variavel var
+b = 200 // É possivel reatribuir o valor de uma variavel let
+
+console.log(a, b)
+
+const c = 5 
+// c = 50 // Não é possivel reatribuir o valor de uma constante
+
+console.log(c)
