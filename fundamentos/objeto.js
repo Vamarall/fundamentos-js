710759025f3f924b4fcda69951b9c542a565c9b4
Victor Amaral Guimarães de Lima Souza <amaralvitor580@gmail.com>
2025-07-25 21:24:35 -0300

    objeto

---
+++ fundamentos/objeto.js
@@ -0,0 +1,14 @@
+const prod = {} // Objeto vazio
+
+prod.nome = 'Celular Ultra Mega'; // Adiciona a propriedade nome
+prod.preco = 4998.90; // Adiciona a propriedade preco
+
+console.log(prod); // Exibe o objeto completo
+
+const prod2 = {
+    nome: 'Camisa Polo',
+    preco: 79.90,
+    desconto: 0.40 // Adiciona a propriedade desconto
+};
+
+console.log(prod2); // Exibe o segundo objeto completo
