01d677b540c3c1af02d774cf0a56f9b026492dcb
Victor Amaral Guimarães de Lima Souza <amaralvitor580@gmail.com>
2025-07-29 19:51:19 -0300

    IIFE

---
+++ funcao/iife.js
@@ -0,0 +1,5 @@
+// IIFE -> Immediately Invoked Function Expression
+
+(function () {
+    console.log('Executando uma IIFE - Immediately Invoked Function Expression');
+})(); // Chamada imediata da função
