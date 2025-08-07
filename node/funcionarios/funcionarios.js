44dc3afcf7f3a5eb2c3fb7a12095906eb60751c5
Victor Amaral Guimarães de Lima Souza <amaralvitor580@gmail.com>
2025-08-07 20:44:36 -0300

    Desafio func

--- node/funcionarios/funcionarios.js
+++ node/funcionarios/funcionarios.js
@@ -1,8 +1,22 @@
 const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json';
 const axios = require('axios'); // axios é uma biblioteca para fazer requisições HTTP
 
+
+const chineses = f => f.pais === 'China';
+const genero = f => f.genero === 'F';
+const menorSalario = ((menor, funcAtual) => {
+    return funcAtual.salario < menor.salario ? funcAtual : menor;
+})
+
 // axios.get(url) faz uma requisição GET para a URL fornecida
 axios.get(url).then(response => {
     const funcionarios = response.data;
     console.log(funcionarios);
+
+    const result = funcionarios.filter(chineses).filter(genero).reduce(menorSalario);
+    console.log(result);
+
 })
+
+
+console.log(menorSalario);
