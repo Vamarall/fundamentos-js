498fffd4db737669369318d7e464fb5807a6216e
Victor Amaral Guimarães de Lima Souza <amaralvitor580@gmail.com>
2025-08-07 21:25:54 -0300

    Passando parametros entre modulos

---
+++ node/passandoParametrosCliente.js
@@ -0,0 +1,4 @@
+// Importando o módulo e passando parâmetros
+const saudacoes = require('./passandoParametros')('Ana', 'Lucas', 'João');
+
+console.log(saudacoes);
--- node/this.js
+++ node/this.js
@@ -1,10 +1,10 @@
 console.log(this === global); // false
 console.log(this === module.exports) // true
 
-function logThis(){
+function logThis() {
     console.log('Dentro da funcao...')
-    console.log(this === module.exports) // false
-    console.log(this === global) //true
+    console.log('Aponta para module.exports: ', this === module.exports) // false
+    console.log('Aponta para global: ', this === global) //true
 }
 
 logThis();
