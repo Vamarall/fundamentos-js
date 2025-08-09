1cfaa7e5ba8ed889cb890d93ca95c35167e1a8e9
Victor Amaral Guimarães de Lima Souza <amaralvitor580@gmail.com>
2025-08-09 19:50:48 -0300

    Process: Entrada e Saida Padrao

---
+++ node/entradaESaida.js
@@ -0,0 +1,17 @@
+// Description: This script reads input from the command line and outputs it to the console.
+const anonimo = process.argv.indexOf('-a') !== -1;
+//console.log(anonimo)
+
+if(anonimo){
+    console.log('Fala anonimo!')
+    process.exit()
+}else{
+    process.stdout.write('Informe seu nome:')
+    process.stdin.on('data', data => {
+        const nome = data.toString().replace('\n', '');
+
+        process.stdout.write(`Fala ${nome}!!\n`);
+        process.exit();
+    })
+
+}
